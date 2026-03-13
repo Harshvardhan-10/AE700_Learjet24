226bcace6f017b4aa7371b53a850c51888968c1e
Harshvardhan-10 <harshvardhan10088@gmail.com>
2026-03-13 21:45:41 +0530

    Completed the code for all tools

--- mavsim_simulink/tools/Quaternion2Rotation.m
+++ mavsim_simulink/tools/Quaternion2Rotation.m
@@ -4,6 +4,9 @@ function R = Quaternion2Rotation(quaternion)
     e1 = quaternion(2);
     e2 = quaternion(3);
     e3 = quaternion(4);
-
-    R = 
+ 
+    R = [...
+        e1^2+e0^2-e2^2-e3^2,   2*(e1*e2-e3*e0),       2*(e1*e3+e2*e0);...
+        2*(e1*e2+e3*e0),        e2^2+e0^2-e1^2-e3^2,   2*(e2*e3-e1*e0);...
+        2*(e1*e3-e2*e0),        2*(e2*e3+e1*e0),        e3^2+e0^2-e1^2-e2^2]; 
 end
